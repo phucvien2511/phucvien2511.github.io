9f7e5fe526b97103a8aa9dd307a0462916086552
minhphucvien <vienphuc2511@gmail.com>
2023-01-11 22:01:07 +0700

    Update menu

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //Load data from JSON to pokedex and moves variable
 import pokedex from './pokedex.json' assert { type: "json" };
 import moves from './moves.json' assert { type: "json" };
+//Add this to parse url as JSON's default format
+for (let i = 0; i < pokedex.length; i++) {
+    pokedex[i].img = pokedex[i].img.toJSON().default;
+}
+
 //Function to display pokedex
 window.onload = function () {
     const displayPokemon = (pokemon) => {
